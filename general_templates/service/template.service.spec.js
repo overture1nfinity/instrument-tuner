c8ddfd3fb13618f13e208d399f7772622d9911c4
overture1nfinity <d.walkerwd7@gmail.com>
2018-01-15 10:55:36 -0700

    unit tests update

---
+++ general_templates/service/template.service.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe(/*@name*/ + '.service', function() {
+    beforeEach(module(/*@module*/));
+
+    var $controller, $rootScope;
+
+    beforeEach(inject(function($controller, $rootScope) {
+        $controller = $controller;
+        $rootScope = $rootScope;
+    }));
+});
