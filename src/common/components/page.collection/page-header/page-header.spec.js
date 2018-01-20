7e576973bcd463914c354c1bf4917f137c73e228
overture1nfinity <d.walkerwd7@gmail.com>
2018-01-20 04:03:08 -0700

    added unit tests

--- src/common/components/page.collection/page-header/page-header.spec.js
+++ src/common/components/page.collection/page-header/page-header.spec.js
@@ -1,10 +1,12 @@
+'use strict';
+
 describe('page-header.view.controller', function() {
     beforeEach(module('common'));
 
     var $controller, $rootScope;
 
-    beforeEach(inject(function(_$controller_, _$rootScope) {
-        $controller = _$controller_;
-        $rootScope = _$rootScope;
+    beforeEach(inject(function($controller, $rootScope) {
+        $controller = $controller;
+        $rootScope = $rootScope;
     }));
 });
