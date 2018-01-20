f5d4139be91618861e60b7212b78dd99ed6cf3df
overture1nfinity <d.walkerwd7@gmail.com>
2018-01-20 04:06:59 -0700

    post unit test

--- src/common/services/factories/audio-math.factory.js
+++ src/common/services/factories/audio-math.factory.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('common').factory('audioMath', AudioMathFactory);
+    angular.module('common').factory('AudioMath', AudioMathFactory);
 
     /** @ngInject */
     function AudioMathFactory(_, AUDIO_CFG, pitchDetect) {
@@ -11,17 +11,23 @@
              * @summary Calculates the amount of cents a frequency is off of a reference pitch.
              * @description
              * Uses this formula: n = 1200*log2(b/a) -- 'a' and 'b' are the 2 subject frequencies.
-             * @param {number} freq The subject frequency in Hz.
              * @param {number} refFreq The frequency of the reference pitch.
+             * @param {number} freq The subject frequency in Hz.
              * @returns {number || null} Null if something went wrong, else cent value.
              */
-            calculatePitchDelta: function(freq, refFreq) {
+            calculatePitchDelta: function(refFreq, freq) {
                 var cents = null;
 
-                if(freq && refFreq) {
-                    cents = 1200*Math.log2(refFreq / freq) * -1; // 100 cents should be the maximum output
+                if(refFreq && freq) {
+                    if(refFreq !== freq) {
+                        cents = 1200*Math.log2(refFreq / freq) * -1; // 100 cents should be the maximum output
+                    }
+
+                    else {
+                        cents = 0; // no difference
+                    }
                 }
-                
+
                 return cents;
             },
 
