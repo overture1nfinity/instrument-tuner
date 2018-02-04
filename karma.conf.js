f33c6e7875179fca88519435b1d30ee47874f1d7
overture1nfinity <d.walkerwd7@gmail.com>
2018-02-04 04:14:01 -0700

    removed unnecessary files

--- karma.conf.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(config) {
-    config.set({
-        basePath: './src',
-        browsers: ['Chrome'],
-        frameworks: ['jasmine'],
-        reporters: ['progress', 'coverage'],
-    });
-}
